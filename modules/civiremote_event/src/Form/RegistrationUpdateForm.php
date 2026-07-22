44658ffd2edf2cdc0239da4e576897cee0954ad0
Dominic Tubach <tubach@systopia.de>
2026-07-22 11:22:47 +0200

    Allow to disable confirmation step

--- modules/civiremote_event/src/Form/RegistrationUpdateForm.php
+++ modules/civiremote_event/src/Form/RegistrationUpdateForm.php
@@ -39,68 +39,14 @@ class RegistrationUpdateForm extends RegisterForm {
   /**
    * @inheritDoc
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $step = $form_state->get('step');
-
-    if (
-      isset($form['actions']['back'])
-      && $form_state->getTriggeringElement()['#value'] == $form['actions']['back']['#value']
-    ) {
-      // Handle backwards navigation.
-      $form_state->set('step', --$step);
-      $form_state->setValues($form_state->get('values'));
-      $form_state->setRebuild();
-    }
-    else {
-      // Handle forward navigation.
-      if ($step == array_search('confirm', $form_state->get('steps'))) {
-        // Submit the form values to the CiviRemote Event API.
-        $values = $form_state->get('values') ?: [];
-        $this->preprocessValues($values);
-        try {
-          $result = $this->cmrf->updateEventRegistration(
-            $this->event->id,
-            $this->profile,
-            $this->remote_token,
-            $values,
-            TRUE
-          );
-
-          // Advance to "Thank you" step (when this is no invitation or it has
-          // been confirmed).
-          if (
-            !array_key_exists('confirm', $this->fields)
-            || !empty($values['confirm'])
-          ) {
-            $form_state->set(
-              'step',
-              array_search('thankyou', $form_state->get('steps'))
-            );
-            $form_state->setRebuild();
-          }
-          else {
-            // Redirect to target from configuration.
-            $config = Drupal::config('civiremote_event.settings');
-            /* @var Url $url */
-            $url = Drupal::service('path.validator')
-              ->getUrlIfValid($config->get('form_redirect_route'));
-            $form_state->setRedirectUrl($url);
-          }
-        }
-        catch (Exception $exception) {
-          $form_state->set('error', TRUE);
-          $form_state->setRebuild();
-        }
-      }
-      else {
-        $form_state->cleanValues();
-        $values = $form_state->getValues();
-        // Advance to next step and rebuild the form.
-        $form_state->set('values', $values);
-        $form_state->set('step', ++$step);
-        $form_state->setRebuild();
-      }
-    }
+  protected function submitCmrf(array $values): array {
+    return $this->cmrf->updateEventRegistration(
+      $this->event->id,
+      $this->profile,
+      $this->remote_token,
+      $values,
+      TRUE
+    );
   }
 
 }
